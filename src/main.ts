78d4e87d4164b5ad4a34cedf653c3da47585adab
Hichem Ben <hichemben@MBP-van-Hichem.home>
2024-07-14 21:27:09 +0200

    fix: app.use(pinia)

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import router from './router'
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 async function initialize() {
